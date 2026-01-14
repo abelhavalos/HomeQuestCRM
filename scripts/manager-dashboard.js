bcaec49eba0e2575afec0dcc7391e0603a72d504
abelhavalos <51245849+abelhavalos@users.noreply.github.com>
2026-01-13 21:52:53 -0800

    Update manager-dashboard.js

--- scripts/manager-dashboard.js
+++ scripts/manager-dashboard.js
@@ -2,7 +2,7 @@
 // Manager Dashboard JS
 // =====================================
 
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzmLgKZBmvAACGmVXb-xYlNlHAAHR7ACcUUeu7Xiw4sQV2w2a7jzIkok0ILXx6QuiCAkQ/exec"; // Replace with your Apps Script URL
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzswOA3wBYD4UV4zG8qZHv1H0PradcU4v9vVkacVLeN2iBjo0FSqM6qgA4HOqRftn1XWA/exec"; // Replace with your Apps Script URL
 
 // -------------------------------
 // GLOBAL STATE
