5190d08428378cd1021550792aa4ac8fab5062d7
abelhavalos <51245849+abelhavalos@users.noreply.github.com>
2026-01-14 18:00:40 -0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const loginMessage = document.getElementById("loginMessage");
   loginMessage.textContent = "";
 
-  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzufjHOh1GDq9RrghDcXZ5qvF4Vp_sC3sl3_JA0HBP81cmrC8I-QOn82LvFG4zhpjSABg/exec";
+  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwu6-aIdFleh8oZdQjRtmv4tsbelsPLS1v2fVMCjDvS_nE7RzWbRQ7Gtpk56sFS8VtwtA/exec";
 
   // -----------------------------
   // ROLE TAB SWITCHING
