6ab8fbafecf3725e17d940c59cc8287c479c8cd4
abelhavalos <51245849+abelhavalos@users.noreply.github.com>
2026-01-14 18:28:50 -0800

    Update forgot-access.js

--- scripts/forgot-access.js
+++ scripts/forgot-access.js
@@ -1,5 +1,5 @@
 const WEB_APP_URL =
-  "https://script.google.com/macros/s/AKfycbwu6-aIdFleh8oZdQjRtmv4tsbelsPLS1v2fVMCjDvS_nE7RzWbRQ7Gtpk56sFS8VtwtA/exec";
+  "https://script.google.com/macros/s/AKfycbw1QyxI16_Xhg3LcghPPcj-cr632yVlMUA4JsYoQyhVJYSnOJepq-kWpkCSco_K-SE_sA/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("sendResetRequest");
